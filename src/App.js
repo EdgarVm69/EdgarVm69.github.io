834be3378192c7628742ec77f6aea6b3bed6980c
EdgarVm69 <137853822+EdgarVm69@users.noreply.github.com>
2023-06-30 19:41:49 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <body>
+      <div class="e1_2">
+        <div  class="e1_6"></div>
+        <div  class="e1_5"></div>
+        <span  class="e3_3">LIONEL ANDRES MESSI CUCCITTINI</span>
+        <span  class="e3_8">Lionel Messi, nacido el 24 de junio de 1987 en Rosario, Argentina, es considerado uno de los mejores futbolistas de todos los tiempos. Desde su infancia, Messi mostró un talento excepcional para el fútbol, y a una edad temprana se unió a las divisiones juveniles del FC Barcelona.</span>
+        <span  class="e3_11">Messi  en este momento, esta en el inter de miami, aun no debuta y esta con 0 goles en el actual club.
+        Ayudanos contando los goles de messi!!!.</span>
+        <a href='Contador' class="learn-more">Hagamoslo</a>
+        </div>
+    </body>
+  );
+}
+
+export default App;
---
+++ src/Page/GolPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../Page/Gol.css';
+
+function GolPage() {
+  return (
+    <body>
+        <div class="e1_11">
+            <div  class="e3_24"></div>
+            <span  class="e3_26">MESSI</span>
+            <div  class="e3_28"></div>
+            <span  class="e3_30">Inter de Miami</span>
+            <span  class="e3_31">Goles en el club: 0 Goles hasta el momento: 793</span>
+            <span  class="e3_32">Fecha del partido:</span>
+            <span>Partido:</span>
+            <a href='/' class="button"><span>GOOOOL</span></a>
+            <div  class="e3_38"></div>
+        </div>
+    </body>
+  );
+}
+
+export default GolPage;
